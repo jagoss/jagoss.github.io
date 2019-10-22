8218beebcddf0228d26c3fcac1317715baebb253
juan <juan.agossweiler@gmail.com>
2019-10-21 23:14:11 -0300

    sigue sin andar

--- js/image_to_txt/image_to_txt.js
+++ js/image_to_txt/image_to_txt.js
@@ -1,6 +1,6 @@
 // 1. After including the Tesseract script, initialize it in the browser
 // Note: to prevent problems while tesseract loads scripts, provide the absolute path to the file from your domain
- window.Tesseract = Tesseract.create({
+window.Tesseract = Tesseract.create({
     // Path to worker
     workerPath: 'https://cdn.rawgit.com/naptha/tesseract.js/0.2.0/dist/worker.js',
     // Path of folder where the language trained data is located
@@ -17,13 +17,13 @@ document.getElementById("img-to-txt").addEventListener("click", function () {
     // Disable button until the text recognition finishes
     btn.disable = true;
 
-    // Configure recognition
-    let tesseractSettings = {
-        lang: 'eng'
-    };
-    var $pdfCanvas = $("#pdf-canvas");
-    var myCanvasContext = $pdfCanvas[0].getContext("2d");
-
+    let img = document.getElementById("cargar_archivo").files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(img);
+    reader.onload = function () {
+        drawImage(reader.result)
+    }
+});
 
 //     Tesseract.recognize(
 //         myCanvasContext,
@@ -34,19 +34,54 @@ document.getElementById("img-to-txt").addEventListener("click", function () {
 //     })
 // });
 //
+//
+
+// });
+// }, false);
 
 
-    // Convert an image to text. This task works asynchronously, so you may show
-    // your user a loading dialog or something like that, or show the progress with Tesseract
-    Tesseract.recognize(myCanvasContext, tesseractSettings).then(function (result) {
-        // The result object of a text recognition contains detailed data about all the text
-        // recognized in the image, words are grouped by arrays etc
-        console.log(result);
+// Convert an image to text. This task works asyn
 
-        // Show recognized text in the browser !
-        alert(result.text);
-    }).finally(function () {
-        // Enable button once the text recognition finishes (either if fails or not)
-        btn.disable = false;
-    });
-}, false);
+
+// Convert an image to text. This task works asynchronously, so you may show
+//     // your user a loading dialog or something like that, or show the progress with Tesseract
+//     Tesseract.recognize(src, tesseractSettings).then(function (result) {
+//         // The result object of a text recognition contains detailed data about all the text
+//         // recognized in the image, words are grouped by arrays etc
+//         console.log(result);
+//
+//         // Show recognized text in the browser !
+//         alert(result.text);
+//     }).finally(function () {
+//         // Enable button once the text recognition finishes (either if fails or not)
+//         btn.disable = false;
+//     });
+// }, false);
+
+function drawImage(url) {
+
+    const canvas = $("#pdf-canvas")[0];
+
+    // Configure recognition
+    let tesseractSettings = {
+        lang: 'eng'
+    };
+
+    let ctx = canvas.getContext('2d');
+    let image = new Image();
+    image.src = url;
+    image.crossOrigin = "Anonymous";
+    image.onload = () => {
+
+        canvas.width = canvas.width;
+        canvas.height = image.height;
+        ctx.drawImage(image, 0, 0);
+
+        let src = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+        Tesseract.recognize(src).then((r) => {
+            alert(r);
+            document.getElementById('ocr_results').value = r.text;
+        })
+    }
+}
