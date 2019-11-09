299dc0ab74f4923852fd7f1d8592a07f9185ac5b
Germán <gabuarab@correo.um.edu.uy>
2019-11-09 17:57:53 -0300

    cambio a una sola pagina

--- public/js/epub/runner.js
+++ public/js/epub/runner.js
@@ -1,6 +1,7 @@
 document.getElementById("convertir").addEventListener("click", function () {
     const fs = require("fs");
-    const text = fs.readFileSync("/texts/mytext.txt");
+    const text = fs.read
+    FileSync("/texts/mytext.txt");
     const textByLine = text.split("\n");
     for (let i = 0; i < textByLine.length; i++) {
         console.log(textByLine[i])
