a29d832b05877fddc376656fe9f5b63eb8397779
Rolo <rodri.lopez98@gmail.com>
2019-11-11 13:11:56 -0300

    Refactor iniciado y comienzo de ingreso de textos

--- server.js
+++ server.js
@@ -3,17 +3,22 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+
     //__dirname : It will resolve to your project folder.
 });
 
-router.get('/about',function(req,res){
-    res.sendFile(path.join(__dirname+'/about.html'));
+router.get('/about', function (req, res) {
+    res.sendFile(path.join(__dirname + '/about.html'));
 });
 
-router.get('/sitemap',function(req,res){
-    res.sendFile(path.join(__dirname+'/sitemap.html'));
+router.get('/sitemap', function (req, res) {
+    res.sendFile(path.join(__dirname + '/sitemap.html'));
+});
+
+app.get('/home', function (req, res) {
+    res.render('/pages/conversion.html');
 });
 
 //add the router
@@ -21,9 +26,8 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 
 //Store all JS and CSS in Scripts folder.
 
-const principal = require('./principal.js');
-app.use('/principal', principal);
 app.use('/', router);
+
 app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
